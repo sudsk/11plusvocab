cee794995b16287c1fb2e6d27894b937b16606b0
sudsk <suds_linux_8@inst-builder-debian-12-build-build-09n69.local>
2025-04-02 22:21:55 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import FlashCardApp from './FlashCardApp';
+
+function App() {
+  return (
+    <div className="App">
+      <FlashCardApp />
+    </div>
+  );
+}
+
+export default App;
